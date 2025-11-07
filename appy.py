1baf0f7fe123f53a811b40768773f8aa9e7be2a2
Jorge09829985 <jdv.escobar@yavirac.edu.ec>
2025-11-06 21:08:33 -0500

    listo

--- appy.py
+++ appy.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hola Mundo desde Docker y CI/CD - Taller 6 (Alan)"
+    return "Hola Mundo desde Docker y CI/CD - Taller 6 (escobar)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
